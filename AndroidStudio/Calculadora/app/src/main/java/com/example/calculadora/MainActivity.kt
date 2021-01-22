b9a57b362c9ec3adafe0780120bc2edea72aedcc
Joao Felipe <joaocamargo2205@gmail.com>
2021-01-22 20:54:38 -0300

    CALCULAR DOIS NUMEROS

---
+++ AndroidStudio/Calculadora/app/src/main/java/com/example/calculadora/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.example.calcular
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import com.example.calculadora.R
+import com.google.android.material.textfield.TextInputEditText
+
+class MainActivity : AppCompatActivity() {
+
+    //realizando findViewById
+    private val primeiroNumEdit by lazy { findViewById<TextInputEditText>(R.id.primerio_num) }
+    private val segundoNumEdit by lazy { findViewById<TextInputEditText>(R.id.segundo_num) }
+    private val txtResultado by lazy { findViewById<TextView>(R.id.txt_resultado) }
+    private val btnCalcular by lazy { findViewById<Button>(R.id.btn_calcular) }
+    // by lazy : quando variavel for chamada ja ira estar inicializada
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        //ao clicar no botao calcular, pega os dois numeros que o usuario escreveu soma eles e insere no TextView
+        btnCalcular.setOnClickListener{
+            TODO("REALIZAR A TRATATIVA DE ERROS QUE USUARIO PODE REALIZAR AO TENTAR CALCULAR")
+            val primeiroNum = primeiroNumEdit.text.toString()
+            val segundoNum = segundoNumEdit.text.toString()
+            txtResultado.text = soma(primeiroNum.toInt(), segundoNum.toInt())
+        }
+    }
+
+    //Pega dois numeros inteiros soma eles e retorna formatado para uma string
+    fun soma(primeiroNum: Int, segundoNum: Int) : String {
+        val resultado = primeiroNum + segundoNum
+        return resultado.toString()
+    }
+}
